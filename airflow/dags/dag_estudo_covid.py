b1c61d2c2ba37e92932120f15aa0fdf8d912615d
Giuliano Vimieiro <gsvimieiro@gmail.com>
2023-06-06 22:44:01 +0000

    dag

--- airflow/dags/dag_estudo_covid.py
+++ airflow/dags/dag_estudo_covid.py
@@ -12,7 +12,7 @@ with DAG(dag_id='trigger_airbyte_job',
 
     airbyte_economy_sf_sync = AirbyteTriggerSyncOperator(
         task_id='airbyte_economy-sf',
-        airbyte_conn_id='poc-airbyte-airflow',
+        airbyte_conn_id='airbyte_connection',
         connection_id='e49a2b45-1615-4e9f-aaf4-4f8677b9a6d4',
         asynchronous=False,
         timeout=3600,
@@ -21,7 +21,7 @@ with DAG(dag_id='trigger_airbyte_job',
 
 #    airbyte_demographics_sf_sync = AirbyteTriggerSyncOperator(
 #        task_id='airbyte_demographics-sf',
-#        airbyte_conn_id='poc-airbyte-airflow',
+#        airbyte_conn_id='airbyte_connection',
 #        connection_id=????,
 #        asynchronous=False,
 #        timeout=3600,
@@ -30,7 +30,7 @@ with DAG(dag_id='trigger_airbyte_job',
 
     airbyte_economy_s3_sync = AirbyteTriggerSyncOperator(
         task_id='airbyte_economy-s3',
-        airbyte_conn_id='poc-airbyte-airflow',
+        airbyte_conn_id='airbyte_connection',
         connection_id='5978d61c-0ae0-4454-9b9a-aeedc3b30849',
         asynchronous=False,
         timeout=3600,
