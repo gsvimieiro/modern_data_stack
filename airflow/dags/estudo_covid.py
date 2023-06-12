a631c2968365603f69a09fd7653ae00cd539e938
Giuliano Vimieiro <gsvimieiro@gmail.com>
2023-06-12 21:10:05 +0000

    dags add

--- airflow/dags/estudo_covid.py
+++ airflow/dags/estudo_covid.py
@@ -1,18 +1,39 @@
 from airflow import DAG
-import airflow.utils.dates
-from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+import json
 
-dag = DAG(
-    dag_id = "hello_world",
-    description="teste",
-    start_date=airflow.utils.dates.days_ago(14),
-    schedule_interval=None,
-)
+with DAG(dag_id='estudo_covid',
+         default_args={'owner': 'airflow'},
+         schedule_interval='@daily',
+         start_date=days_ago(1)
+         ) as dag:
 
-task_echo_message = BashOperator(
-    task_id="echo_message",
-    bash_command="echo teste",
-    dag=dag
-)
+    airbyte_economy_sf_sync = AirbyteTriggerSyncOperator(
+        task_id='airbyte_economy-sf',
+        airbyte_conn_id='airbyte_connection',
+        connection_id='74d2b99a-98cd-4182-a2e9-972adbfc778a',
+        asynchronous=False,
+        timeout=3600,
+        wait_seconds=3
+    )
 
-task_echo_message
+#    airbyte_demographics_sf_sync = AirbyteTriggerSyncOperator(
+#        task_id='airbyte_demographics-sf',
+#        airbyte_conn_id='airbyte_connection',
+#        connection_id=????,
+#        asynchronous=False,
+#        timeout=3600,
+#        wait_seconds=3
+#    )
+
+    airbyte_economy_s3_sync = AirbyteTriggerSyncOperator(
+        task_id='airbyte_economy-s3',
+        airbyte_conn_id='airbyte_connection',
+        connection_id='d39aaba2-c421-40dc-bf78-b38d3f5611dd',
+        asynchronous=False,
+        timeout=3600,
+        wait_seconds=3
+    )
+
+    airbyte_economy_sf_sync >> airbyte_economy_s3_sync
